abd8df15a15584d92831339a1b156401a25ee1f2
gunarto90 <gunarto.nctu@gmail.com>
2018-07-19 14:31:44 +0800

    Added run_fvs.sh

---
+++ run_fvs.sh
@@ -0,0 +1,3 @@
+java -Xmx16g -jar "target/FeatureValue-1.0.jar" "./dataset/run_uci_small" 10 "config_uci.json"
+java -Xmx16g -jar "target/FeatureValue-1.0.jar" "./dataset/run_uci_mid" 10 "config_uci.json"
+java -Xmx16g -jar "target/FeatureValue-1.0.jar" "./dataset/run_uci_big" 10 "config_uci.json"
